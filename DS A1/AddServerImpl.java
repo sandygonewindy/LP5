8ef094cd35a4b6de38151c29bef223ef860545df
Sudeep <93223422+sandygonewindy@users.noreply.github.com>
2023-05-28 23:28:47 +0530

    Add files via upload

---
+++ DS A1/AddServerImpl.java
@@ -0,0 +1,10 @@
+import java.rmi.*;
+import java.rmi.server.UnicastRemoteObject;
+public class AddServerImpl extends UnicastRemoteObject implements AddServerIntf{
+    public AddServerImpl() throws RemoteException{
+
+    }
+    public double add(double d1, double d2) throws RemoteException{
+        return d1 + d2;
+    }
+}
